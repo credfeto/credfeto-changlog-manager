5ccd9ce4d2473900037cbdf619a6ed212cf2fc8e
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-03-14 11:54:43 +0000

    Additional text

--- src/Credfeto.ChangeLog.Tests/ChangeLogUpdaterAddEntryTests.cs
+++ src/Credfeto.ChangeLog.Tests/ChangeLogUpdaterAddEntryTests.cs
@@ -21,6 +21,9 @@ public sealed class ChangeLogUpdaterAddEntryTests : TestBase
             @"# Changelog
 All notable changes to this project will be documented in this file.
 
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
 <!--
 Please ADD ALL Changes to the UNRELEASED SECTION and not a specific release
 -->
@@ -30,7 +33,9 @@ Please ADD ALL Changes to the UNRELEASED SECTION and not a specific release
 - Added a new entry
 ### Fixed
 ### Changed
+### Deprecated
 ### Removed
+### Security
 ### Deployment Changes
 
 <!--
