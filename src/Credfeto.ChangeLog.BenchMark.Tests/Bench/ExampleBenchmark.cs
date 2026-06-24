c9e9e64bf4bb19475e1437b301bb51c12f720ddf
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-06-24 13:59:22 +0000

    Cleanup: ExampleBenchmark.cs

--- src/Credfeto.ChangeLog.BenchMark.Tests/Bench/ExampleBenchmark.cs
+++ src/Credfeto.ChangeLog.BenchMark.Tests/Bench/ExampleBenchmark.cs
@@ -5,11 +5,7 @@ namespace Credfeto.ChangeLog.BenchMark.Tests.Bench;
 
 [SimpleJob]
 [MemoryDiagnoser(false)]
-[SuppressMessage(
-    category: "codecracker.CSharp",
-    checkId: "CC0091:MarkMembersAsStatic",
-    Justification = "Benchmark"
-)]
+[SuppressMessage(category: "codecracker.CSharp", checkId: "CC0091:MarkMembersAsStatic", Justification = "Benchmark")]
 [SuppressMessage(
     category: "FunFair.CodeAnalysis",
     checkId: "FFS0012: Make sealed static or abstract",
